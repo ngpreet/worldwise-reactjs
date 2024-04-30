5f9678cd12eb64d9e4ac6ed20451ddf1058a247e
ngpreet <ng.navpreet@gmail.com>
2024-04-30 09:47:10 +0530

    state updates, useReducer with context

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,69 +1,127 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
 import * as PropTypes from "prop-types";
 
 const CitiesContext = createContext();
 
 const BASE_URL = "http://localhost:8000";
 
+const initialState = {
+  cities: [],
+  isLoading: false,
+  currentCity: {},
+  error: "",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "loading":
+      return { ...state, isLoading: true };
+    case "cities/loaded":
+      return { ...state, isLoading: false, cities: action.payload };
+    case "city/loaded":
+      return {
+        ...state,
+        isLoading: false,
+        currentCity: action.payload,
+      };
+    case "city/created":
+      return {
+        ...state,
+        isLoading: false,
+        cities: [...state.cities, action.payload],
+        currentCity: action.payload,
+      };
+    case "city/deleted":
+      return {
+        ...state,
+        isLoading: false,
+        cities: state.cities.filter((city) => city.id !== action.payload),
+      };
+    case "rejected":
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload,
+      };
+    default:
+      throw new Error("Unknown action type!");
+  }
+}
+
 function CitiesProvider({ children }) {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [currentCity, setCurrentCity] = useState({});
+  const [{ cities, isLoading, currentCity }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   async function fetchCities() {
-    setIsLoading(true);
+    dispatch({ type: "loading" });
     const res = await fetch(`${BASE_URL}/cities`);
     const data = await res.json();
-    setCities(data);
-    setIsLoading(false);
+    dispatch({ type: "cities/loaded", payload: data });
   }
 
   useEffect(function () {
     try {
       fetchCities();
     } catch (error) {
-      setIsLoading(false);
+      dispatch({
+        type: "rejected",
+        payload: "Error occurred while loading cities!",
+      });
     }
   }, []);
 
   async function getCity(id) {
     try {
-      setIsLoading(true);
+      dispatch({ type: "loading" });
       const res = await fetch(`${BASE_URL}/cities/${id}`);
       const data = await res.json();
-      setCurrentCity(data);
-      setIsLoading(false);
+      dispatch({ type: "city/loaded", payload: data });
     } catch (error) {
-      setIsLoading(false);
+      dispatch({
+        type: "rejected",
+        payload: "Error occurred while loading city!",
+      });
     }
   }
 
   async function createCity(newCity) {
     try {
-      setIsLoading(true);
+      dispatch({ type: "loading" });
       const res = await fetch(`${BASE_URL}/cities`, {
         method: "POST",
         body: JSON.stringify(newCity),
       });
       const data = await res.json();
-      setCities((cities) => [...cities, data]);
-      setIsLoading(false);
+      dispatch({ type: "city/created", payload: data });
     } catch (error) {
-      setIsLoading(false);
+      dispatch({
+        type: "rejected",
+        payload: "Error occurred while creating city!",
+      });
     }
   }
 
   async function deleteCity(id) {
     try {
-      setIsLoading(true);
+      dispatch({ type: "loading" });
       const res = await fetch(`${BASE_URL}/cities/${id}`, {
         method: "DELETE",
       });
       await res.json();
-      setCities((cities) => cities.filter((city) => city.id !== id));
-      setIsLoading(false);
+      dispatch({ type: "city/deleted", payload: id });
     } catch (error) {
-      setIsLoading(false);
+      dispatch({
+        type: "rejected",
+        payload: "Error occurred while deleting city!",
+      });
     }
   }
 
@@ -75,7 +133,7 @@ function CitiesProvider({ children }) {
         currentCity,
         getCity,
         createCity,
-        deleteCity
+        deleteCity,
       }}
     >
       {children}
